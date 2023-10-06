3702f76282918435f819fb74bf337522b612fcdb
perceptkraft <141613035+perceptkraft@users.noreply.github.com>
2023-10-06 14:19:21 +0530

    Setup

---
+++ app/layout.tsx
@@ -0,0 +1,25 @@
+import "./globals.css";
+import { Inter } from "next/font/google";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata = {
+  title: "Karan Dodeja | Personal Portfolio",
+  description: "A Fullstack Developer!",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className={`${inter.className} bg-gray-50 text-gray-950`}>
+        <div className="bg-[#fde2e3] relative top-[-6rem] right-[11rem] -z-10 h-[31.25rem] w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem]"></div>
+        <div className="bg-[#dbd7fb] absolute top-[-1rem] left-[-35rem] -z-10 h-[31.25rem] w-[50rem] rounded-full blur-[10rem] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem]"></div>
+        {children}
+      </body>
+    </html>
+  );
+}
